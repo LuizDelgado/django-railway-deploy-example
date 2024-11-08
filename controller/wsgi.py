ed29056c641f5777b8614c3b2201367b771e3f59
Luix <luiz.delgado@eniac.edu.br>
2024-11-08 20:37:36 -0300

    Railway Deploy

--- controller/wsgi.py
+++ controller/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import pymysql
+pymysql.install_as_MySQLdb()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controller.settings')
 
 application = get_wsgi_application()
